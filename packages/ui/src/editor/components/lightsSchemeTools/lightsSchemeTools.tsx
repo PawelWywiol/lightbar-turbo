ce19c9515e59fad9c47a3683e1c4938e733578a0
Paweł Wywioł <pawel.wywiol@gmail.com>
2023-11-17 23:17:17 +0100

    lights scheme tool bar

--- packages/ui/src/editor/components/lightsSchemeTools/lightsSchemeTools.tsx
+++ packages/ui/src/editor/components/lightsSchemeTools/lightsSchemeTools.tsx
@@ -1,26 +1,18 @@
 import { LIGHTS_FRAME_SIZES } from 'config';
 
-import { Select } from '../../../select/select';
+import { DropDownMenu } from '../../../dropdownMenu/dropdownMenu';
 
 import type { EditorProps } from '../../editor.types';
 
 export const LightsSchemeTools = ({ scheme, setScheme }: EditorProps) => (
   <div className="flex justify-between content-center px-4">
     <div></div>
-    <div>
-      <Select
-        options={LIGHTS_FRAME_SIZES.map((option) => ({
-          value: `${option.value}`,
-          label: option.label,
+    <div className='flex'>
+      <DropDownMenu
+        options={LIGHTS_FRAME_SIZES.map((size) => ({
+          label: size.label,
+          onClick: () => setScheme({ ...scheme, size }),
         }))}
-        value={`${scheme.size.value}`}
-        onChange={(value) => {
-          const size = LIGHTS_FRAME_SIZES.find(
-            (option) => option.value === Number.parseInt(value, 10),
-          );
-
-          size && setScheme({ ...scheme, size });
-        }}
       />
     </div>
   </div>
