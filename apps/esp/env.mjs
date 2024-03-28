819f24d7ee4ba4b0799347ea80589d15e8e69ef8
Paweł Wywioł <pawel.wywiol@gmail.com>
2024-03-28 22:52:02 +0100

    Update env configuration to use Vite environment variables

--- apps/esp/env.mjs
+++ apps/esp/env.mjs
@@ -2,7 +2,7 @@
  * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
  */
 const env = {
-  DEFAULT_WS_URL: process.env.DEFAULT_WS_URL,
+  DEFAULT_WS_URL: import.meta.env.VITE_DEFAULT_WS_URL,
 };
 
 export { env };
