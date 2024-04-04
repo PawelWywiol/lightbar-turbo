f8fe508e60320fa280bb39fc8e28744e512ab55e
Paweł Wywioł <pawel.wywiol@gmail.com>
2024-04-04 22:38:59 +0200

    Update ESLint configuration

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['plugin:@next/next/recommended', 'custom-next'],
+  extends: ['custom-next'],
   parserOptions: {
     project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname,
