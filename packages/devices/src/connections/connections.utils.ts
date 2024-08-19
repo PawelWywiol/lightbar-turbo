2a255ed409fc1eb269f5897074ab06156d84126e
Paweł Wywioł <pawel.wywiol@gmail.com>
2024-08-19 17:06:54 +0200

    refactor: improve type checking in connections.utils.ts

--- packages/devices/src/connections/connections.utils.ts
+++ packages/devices/src/connections/connections.utils.ts
@@ -1,21 +1,19 @@
 import type { ConnectionResponseData } from './connections.types';
 
 const isConnectionResponseData = (data: unknown): data is ConnectionResponseData => {
-  if (typeof data === 'object' && data !== null) {
-    const { type, data: responseData } = data as ConnectionResponseData;
-    if (type === 'info' && typeof responseData === 'object' && responseData !== null) {
-      const { leds, space, heap, ap } = responseData;
-      if (
-        typeof leds === 'number' &&
-        typeof ap === 'string' &&
-        typeof space === 'number' &&
-        typeof heap === 'number'
-      ) {
-        return true;
-      }
-    }
+  if (typeof data !== 'object' || data === null) {
+    return false;
   }
-  return false;
+
+  const { type, data: responseData } = data as ConnectionResponseData;
+
+  if (type !== 'info' || typeof responseData !== 'object' || responseData === null) {
+    return false;
+  }
+
+  const { leds, ap } = responseData;
+
+  return !!(typeof leds === 'number' && typeof ap === 'string');
 };
 
 export const parseSafeConnectionResponseData = (
