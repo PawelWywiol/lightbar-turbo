37cf0f2a0dc7184a25e0d49d7402f677cf30f6ec
Paweł Wywioł <pawel.wywiol@gmail.com>
2024-09-04 20:28:37 +0200

    refactor: update embedded/esp submodule and improve type checking in connections.utils.ts

--- packages/devices/src/connections/connections.utils.ts
+++ packages/devices/src/connections/connections.utils.ts
@@ -1,4 +1,6 @@
 import {
+  CONNECTION_REQUEST_EOL_INFO,
+  CONNECTION_REQUEST_INFO_LENGTH,
   CONNECTION_REQUEST_SIZE_INFO_LENGTH,
   CONNECTION_REQUEST_TYPE,
   CONNECTION_REQUEST_TYPE_INFO_LENGTH,
@@ -31,26 +33,24 @@ export const connectionRequestWifiBinaryData = (
 ) => {
   const { ssid, password } = data.data;
 
-  const size =
-    CONNECTION_REQUEST_TYPE_INFO_LENGTH +
-    CONNECTION_REQUEST_SIZE_INFO_LENGTH +
-    SSID_MAX_LENGTH +
-    PASSWORD_MAX_LENGTH;
+  const size = CONNECTION_REQUEST_INFO_LENGTH + SSID_MAX_LENGTH + PASSWORD_MAX_LENGTH;
 
   const buffer = new Uint8Array(size);
   const view = new DataView(buffer.buffer);
 
-  view.setUint32(0, CONNECTION_REQUEST_TYPE.wifi);
-  view.setUint32(CONNECTION_REQUEST_SIZE_INFO_LENGTH, size);
+  view.setUint32(0, CONNECTION_REQUEST_TYPE.wifi, true);
+  // view.setUint32(0, CONNECTION_REQUEST_TYPE.wifi); but reverse the bytes, so it's 0x69_66_69_77 instead of 0x77_69_66_69
+  view.setUint32(CONNECTION_REQUEST_SIZE_INFO_LENGTH, size - CONNECTION_REQUEST_INFO_LENGTH, true);
 
-  buffer.set(
-    new TextEncoder().encode(ssid),
-    CONNECTION_REQUEST_TYPE_INFO_LENGTH + CONNECTION_REQUEST_SIZE_INFO_LENGTH,
-  );
-  buffer.set(
-    new TextEncoder().encode(password),
-    CONNECTION_REQUEST_TYPE_INFO_LENGTH + CONNECTION_REQUEST_SIZE_INFO_LENGTH + SSID_MAX_LENGTH,
-  );
+  let offset = CONNECTION_REQUEST_TYPE_INFO_LENGTH + CONNECTION_REQUEST_SIZE_INFO_LENGTH;
+
+  buffer.set(new TextEncoder().encode(ssid), offset);
+  offset += SSID_MAX_LENGTH;
+
+  buffer.set(new TextEncoder().encode(password), offset);
+  offset += PASSWORD_MAX_LENGTH;
+
+  view.setUint32(offset, CONNECTION_REQUEST_EOL_INFO, true);
 
   return buffer;
 };
