7a456f31a5fdc76f7bdc0b825701992b1cc56b01
Paweł Wywioł <pawel.wywiol@gmail.com>
2024-11-03 18:31:27 +0100

    chore: commit lint

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,32 +1,17 @@
 module.exports = {
-  'apps/esp/**/*': () => [
-    'turbo run lint --filter esp',
-    'turbo run type-check --filter esp',
-    'turbo run test:changed --filter esp',
-  ],
-  'apps/web/**/*.ts': () => [
-    'turbo run lint --filter web',
-    'turbo run type-check --filter web',
-    'turbo run test:changed --filter web',
-  ],
-  'packages/config/**/*.tsx': () => [
+  'apps/esp/**/*': () => ['turbo run lint --filter esp', 'turbo run type-check --filter esp'],
+  'apps/web/**/*': () => ['turbo run lint --filter web', 'turbo run type-check --filter web'],
+  'packages/config/**/*': () => [
     'turbo run lint --filter config',
     'turbo run type-check --filter config',
-    'turbo run test:changed --filter config',
   ],
-  'packages/devices/**/*.tsx': () => [
+  'packages/devices/**/*': () => [
     'turbo run lint --filter devices',
     'turbo run type-check --filter devices',
-    'turbo run test:changed --filter devices',
   ],
-  'packages/ui/**/*.tsx': () => [
-    'turbo run lint --filter ui',
-    'turbo run type-check --filter ui',
-    'turbo run test:changed --filter ui',
-  ],
-  'packages/utils/**/*.tsx': () => [
+  'packages/ui/**/*': () => ['turbo run lint --filter ui', 'turbo run type-check --filter ui'],
+  'packages/utils/**/*': () => [
     'turbo run lint --filter utils',
     'turbo run type-check --filter utils',
-    'turbo run test:changed --filter utils',
   ],
 };
