774d81fbbdafc1bdb42815ce7c13f612568ea9c3
Paweł Wywioł <pawel.wywiol@gmail.com>
2024-12-31 13:10:31 +0100

    chore: add frame data handling to connection request processing

--- packages/devices/src/connections/connections.utils.ts
+++ packages/devices/src/connections/connections.utils.ts
@@ -1,5 +1,8 @@
 import {
   CONNECTION_REQUEST_EOL_INFO,
+  CONNECTION_REQUEST_FRAME_COLOR_LENGTH,
+  CONNECTION_REQUEST_FRAME_TEMPO_LENGTH,
+  CONNECTION_REQUEST_FRAME_TYPE_LENGTH,
   CONNECTION_REQUEST_INFO_LENGTH,
   CONNECTION_REQUEST_SIZE_INFO_LENGTH,
   CONNECTION_REQUEST_TYPE,
@@ -54,6 +57,44 @@ export const resolveConnectionRequestWifiBinaryData = (
   return buffer;
 };
 
+export const resolveConnectionRequestFrameBinaryData = (
+  data: Extract<ConnectionRequestData, { type: 'frame' }>,
+) => {
+  const { type, tempo, colors } = data.data;
+
+  const size =
+    CONNECTION_REQUEST_INFO_LENGTH +
+    CONNECTION_REQUEST_FRAME_TYPE_LENGTH +
+    CONNECTION_REQUEST_FRAME_TEMPO_LENGTH +
+    colors.length * CONNECTION_REQUEST_FRAME_COLOR_LENGTH;
+
+  const buffer = new Uint8Array(size);
+  const view = new DataView(buffer.buffer);
+
+  view.setUint32(0, CONNECTION_REQUEST_TYPE.frame, true);
+  view.setUint32(CONNECTION_REQUEST_SIZE_INFO_LENGTH, size - CONNECTION_REQUEST_INFO_LENGTH, true);
+
+  let offset = CONNECTION_REQUEST_TYPE_INFO_LENGTH + CONNECTION_REQUEST_SIZE_INFO_LENGTH;
+
+  view.setUint8(offset, type);
+  offset += CONNECTION_REQUEST_FRAME_TYPE_LENGTH;
+
+  view.setUint8(offset, tempo);
+  offset += CONNECTION_REQUEST_FRAME_TEMPO_LENGTH;
+
+  colors.forEach((color, index) => {
+    view.setUint8(offset + index * CONNECTION_REQUEST_FRAME_COLOR_LENGTH, color);
+  });
+
+  view.setUint32(
+    offset + colors.length * CONNECTION_REQUEST_FRAME_COLOR_LENGTH,
+    CONNECTION_REQUEST_EOL_INFO,
+    true,
+  );
+
+  return buffer;
+};
+
 export const connectionRequestDataToBinaryData = (
   requests: ConnectionRequestData[],
 ): Uint8Array => {
@@ -65,19 +106,7 @@ export const connectionRequestDataToBinaryData = (
         return resolveConnectionRequestWifiBinaryData(requestData);
       }
       case 'frame': {
-        const { type, tempo, colors } = requestData.data;
-
-        const buffer = new Uint8Array(4 + 4 + 4 + colors.length * 4);
-        const view = new DataView(buffer.buffer);
-
-        view.setUint32(0, CONNECTION_REQUEST_TYPE.frame);
-
-        view.setUint32(4, type);
-        view.setUint32(8, tempo);
-
-        colors.forEach((color, index) => view.setUint32(12 + index * 4, color));
-
-        return buffer;
+        return resolveConnectionRequestFrameBinaryData(requestData);
       }
       default: {
         const x: never = requestDataType;
